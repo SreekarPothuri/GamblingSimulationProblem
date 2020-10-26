cce960c4fd5e1d1c244c57fb7d2c2a523d16215b
Your Name <you@example.com>
2020-10-26 18:39:34 +0530

    Added: Stake and bet daily

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -1,6 +1,10 @@
 package com.gamblingsimulation;
 
 public class GamblingSimulation {
+
+	public static final int STAKE = 100;
+	public static final int BET = 1;
+
 	public static void main(String[] args) {
 		System.out.println("*****WELCOME TO GAMBLING SIMULATION PROBLEM*****");
 	}
