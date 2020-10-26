555c32ba50901794aa3dde99d7f792bf669da4ac
Your Name <you@example.com>
2020-10-26 19:36:59 +0530

    Added: Resign Stake method

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -6,7 +6,8 @@ public class GamblingSimulation {
 
 	public static final int STAKE = 100;
 	public static final int BET = 1;
-	public int stake=100;
+	public static final float STAKE_VALUE = 0.5f;
+	public int stake=100,loosingAmount,winningAmount;
 	
 	public int winOrLoss() {
 		Random random = new Random();
@@ -21,10 +22,27 @@ public class GamblingSimulation {
 		return stake;
 	}
 	
+	public int resignStake() {
+		loosingAmount = (int)Math.round(STAKE * STAKE_VALUE);
+		winningAmount = (int)Math.round(STAKE + (STAKE * STAKE_VALUE));
+		boolean stop = true;
+		stake = STAKE;
+		while(stop == true) {
+			winOrLoss();
+			if(stake == loosingAmount) {
+				stop = false;
+			}
+			if(stake == winningAmount){
+				stop = false;
+			}
+		}
+		return stake;
+	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("*****WELCOME TO GAMBLING SIMULATION PROBLEM*****");
 		GamblingSimulation gambling = new GamblingSimulation();
-		System.out.println("Remaining Stake: "+gambling.winOrLoss());
+		System.out.println("Total Remaining Stake: "+gambling.resignStake());
 	}
 }
